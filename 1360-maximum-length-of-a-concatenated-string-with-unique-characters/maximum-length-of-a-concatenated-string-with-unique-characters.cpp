fdab65f63fd8f50057c2e24053dc0fd0f96014aa
Palak <malikpalak1332@gmail.com>
2025-01-27 14:50:20 +0530

    Time: 53 ms (48.49%) | Memory: 61.7 MB (42.45%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,48 @@
+class Solution {
+
+bool compare(vector<int> &selected,string& currentstring){
+    vector<int> selfcheck(26,0);
+    for(int i=0;i<currentstring.size();i++){
+        if(selfcheck[currentstring[i]-'a']==1){
+            return false;
+        }
+        selfcheck[currentstring[i]-'a']=1;
+    }
+    for(int i=0;i<currentstring.size();i++){
+        if(selected[currentstring[i]-'a']==1){
+            return false;
+        }
+    }
+        return true;
+     
+}
+int help(int i,vector<string>&arr,vector<int>&selected,int len){
+    if(i==arr.size()){
+        return len;
+    }
+    string currentstring=arr[i];
+    if(compare(selected,currentstring)==false){
+        return help(i+1,arr,selected,len);
+    }else{
+        //pick
+        for(int j=0;j<currentstring.size();j++){
+            selected[currentstring[j]-'a']=1;
+        }
+        len+=currentstring.size();
+        int op1=help(i+1,arr,selected,len);
+
+        //skip
+        for(int j=0;j<currentstring.size();j++){
+            selected[currentstring[j]-'a']=0;
+        }
+        len-=currentstring.size();
+        int op2=help(i+1,arr,selected,len);
+        return max(op1,op2);
+    }
+}
+public:
+    int maxLength(vector<string>& arr) {
+        vector<int> selected(26,0);
+        return help(0,arr,selected,0);
+    }
+};
