9ac3d40e5925ad2706176f2dbf2cf9321a9aae6b
Palak <malikpalak1332@gmail.com>
2025-01-28 12:27:26 +0530

    Time: 7 ms (23.55%) | Memory: 12.1 MB (23.02%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+bool issafe(int i,int j,int n,vector<vector<char>>&chessboard){
+    //up
+    int tempi=i;
+    int tempj=j;
+    while(tempi>=0){
+        if(chessboard[tempi][j]=='Q'){
+            return false;
+        }
+        tempi--;
+    }
+    //up left
+    tempi=i;
+    tempj=j;
+    while(tempi>=0 && tempj>=0){
+        if(chessboard[tempi][tempj]=='Q'){
+            return false;
+        }
+        tempi--;
+        tempj--;
+    }
+
+    //up right
+    tempi=i;
+    tempj=j;
+    while(tempi>=0 && tempj<=n-1){
+        if(chessboard[tempi][tempj]=='Q'){
+            return false;
+        }
+        tempi--;
+        tempj++;
+    }
+
+
+    return true;
+    
+}
+void help(int i,int n,vector<vector<char>>&chessboard,vector<vector<string>>&ans){
+    if(i==n){
+        vector<string>temp;
+        for(int j=0;j<n;j++){
+            string st="";
+            for(int k=0;k<n;k++){
+                st.push_back(chessboard[j][k]);
+            }
+            temp.push_back(st);
+        }
+        ans.push_back(temp);
+        return;
+    }
+    for(int j=0;j<n;j++){
+        if(issafe(i,j,n,chessboard)){
+            chessboard[i][j]='Q';
+            help(i+1,n,chessboard,ans);
+            chessboard[i][j]='.';
+        }
+    }
+}
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<char>>chessboard(n,vector<char>(n,'.'));
+        vector<vector<string>>ans;
+        help(0,n,chessboard,ans);
+        return ans;
+    }
+};
