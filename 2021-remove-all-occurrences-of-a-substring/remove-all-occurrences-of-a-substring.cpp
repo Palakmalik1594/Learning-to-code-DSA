56e9281d0543cff37bcf2100a13ac0777614a8a4
Palak <malikpalak1332@gmail.com>
2025-02-11 10:36:54 +0530

    Time: 4 ms (15.12%) | Memory: 9.4 MB (68.15%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -3,7 +3,7 @@ public:
     string removeOccurrences(string s, string part) {
         int n=s.length();
        while((s.find(part))!=string::npos){
-        unsigned index=s.find(part);
+        int index=s.find(part);
         s.erase(index,part.size());
        }
        return s;
