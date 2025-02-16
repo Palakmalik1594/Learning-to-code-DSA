45bc0ba6bad75fdd350a758432db8e9348366760
Palak <malikpalak1332@gmail.com>
2025-02-16 20:54:46 +0530

    Time: 2 ms (33.33%) | Memory: 8.6 MB (64.94%) - LeetSync

---
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool findlargest(vector<int>&res,vector<bool>& used,int pos,int n){
+        if(pos==2*n-1){
+            return true;
+        }
+        if(res[pos]!=0){
+            return findlargest(res,used,pos+1,n);
+        }
+        for(int i=n;i>=1;i--){
+            if(used[i]==true){
+                continue;
+            }
+            used[i]=true;
+            res[pos]=i;
+            if(i==1 and findlargest(res,used,pos+1,n)){
+                return true;
+            }
+            if(i>1 and (pos+i)<(2*n-1) and res[pos+i]==0){
+                res[pos+i]=i;
+                if(findlargest(res,used,pos+1,n))
+                return true;
+                res[pos+i]=0;
+            }
+            used[i]=false;
+            res[pos]=0;
+
+        }
+        return false;
+    }
+    vector<int> constructDistancedSequence(int n) {
+        vector<int>res(2*n-1,0);
+        vector<bool> used(n+1,false);
+        findlargest(res,used,0,n);
+        return res;
+    }
+};
