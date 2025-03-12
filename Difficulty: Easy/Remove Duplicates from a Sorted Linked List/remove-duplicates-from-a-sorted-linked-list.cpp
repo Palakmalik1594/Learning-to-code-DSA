dfc4cb3fdfa12df431c004838547347a80a23ca0
Palak <malikpalak1332@gmail.com>
2025-03-12 16:49:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.cpp
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+   // Redirecting input from file
+  freopen("/Users/debojyoti.mandal/expt/input.txt", "r", stdin);
+
+  // Redirecting output to file
+  freopen("/Users/debojyoti.mandal/expt/output.txt", "w", stdout);
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void print(Node *root) {
+    Node *temp = root;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+
+// } Driver Code Ends
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+class Solution {
+  public:
+    // Function to remove duplicates from sorted linked list.
+    Node* removeDuplicates(Node* head) {
+         Node* temp=head;
+         set<int>s;
+         while(temp!=NULL){
+             s.insert(temp->data);
+             temp=temp->next;
+         }
+         temp=head;
+         auto it=s.begin();
+         while(temp!=NULL && it!=s.end()){
+             temp->data=*it;
+             it++;
+             if(temp==NULL || it==s.end()){
+                 temp->next=NULL;
+                 break;
+             }
+             temp=temp->next;
+         }
+         return head;
+        // code here
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    // your code goes here
+    int T;
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+        Solution obj;
+        int data = arr[0];
+        Node *head = new Node(data);
+        Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        Solution ob;
+        Node *result = ob.removeDuplicates(head);
+        print(result);
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
