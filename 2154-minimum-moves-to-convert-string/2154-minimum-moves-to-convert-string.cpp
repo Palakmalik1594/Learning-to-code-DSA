68a8855110a23c69e34912cec94e1e5f49d8adeb
Palak <malikpalak1332@gmail.com>
2025-05-03 22:51:07 +0530

    Time: 0 ms (100%), Space: 8.7 MB (63.33%) - LeetHub

---
+++ 2154-minimum-moves-to-convert-string/2154-minimum-moves-to-convert-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int count=0;
+        for(int i=0;i<s.length();){
+            if(s[i]=='X'){
+              count++;
+              i+=3;
+            }else{
+                i++;
+            }
+        }
+        return count;
+    }
+};
