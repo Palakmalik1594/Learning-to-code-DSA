eb330592a94bccdff323d1bf7306248849325c75
Palak <malikpalak1332@gmail.com>
2025-05-12 16:07:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-       
-        int count=0;
+        int i=0;
+        int j=0;
+        int maxi=0;
         for(int i=0;i<nums.size();i++){
             int sum=0;
-            for(int j=i;j<nums.size();j++){
+            for(int j=0;j<nums.size();j++){
                 sum+=nums[j];
-            
-            if(sum==k){
-                count++;
+
+                if(sum==k){
+                    maxi=max(maxi,j-i+1);
+                }
             }
         }
-        }
-       
-        return count;
+        return maxi;
     }
 };
