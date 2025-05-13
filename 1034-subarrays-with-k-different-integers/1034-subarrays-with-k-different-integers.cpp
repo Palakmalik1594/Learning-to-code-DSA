06d988b4dae1a8c69163a4239d38aaf1e91949e3
Palak <malikpalak1332@gmail.com>
2025-05-13 20:06:14 +0530

    Time: 42 ms (93.35%), Space: 47.9 MB (94.65%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        int n = nums.size();
-        int i = 0, j = 0;
-        int count = 0;
-        unordered_map<int, int> mp;
-        int prefix = 0;
-
-        while (j < n) {
+        int i=0;
+        int j=0;
+        int count=0;
+        int prefix=0;
+        unordered_map<int,int>mp;
+        while(j<nums.size()){
             mp[nums[j]]++;
-
-            if (mp.size() > k) {
-                // Shrink from left
-                while (mp.size() > k) {
-                    mp[nums[i]]--;
-                    if (mp[nums[i]] == 0)
-                        mp.erase(nums[i]);
-                    i++;
+            if(mp.size()>k ){
+                while(mp.size()>k){
+                        mp[nums[i]]--;
+                        if(mp[nums[i]]==0){
+                            mp.erase(nums[i]);
+                        }
+                        i++;
                 }
-                prefix = 0; // reset prefix
-            }
-
-            if (mp.size() == k) {
-                // Count duplicates from the left
-                while (mp[nums[i]] > 1) {
-                    mp[nums[i]]--;
-                    i++;
-                    prefix++;
+                
+                prefix=0;
                 }
+                if(mp.size()==k){
+                        while(mp[nums[i]]>1){
+                            mp[nums[i]]--;
+                            i++;
+                            prefix++;
+                        }
+                    
+                count+=1+prefix;
+              
 
-                count += 1 + prefix;
+                    
+                }
+                j++;
 
-                // Restore for next iteration
-                // Do not move `i` here — keep the window valid
-            }
-
-            j++;
+            
         }
-
         return count;
     }
 };
