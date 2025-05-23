b46c7e83fd615defd3c14e0254251a0e4a2a8456
Palak <malikpalak1332@gmail.com>
2025-05-23 15:39:03 +0530

    Time: 0 ms (100%), Space: 12 MB (57.41%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>mp;
+        for(int ch:arr){
+            mp[ch]++;
+        }
+        vector<int>v;
+        for(auto it:mp){   
+            v.push_back(it.second);
+        }
+        sort(v.begin(),v.end());
+        for(int i=0;i<v.size()-1;i++){
+            if(v[i]==v[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
