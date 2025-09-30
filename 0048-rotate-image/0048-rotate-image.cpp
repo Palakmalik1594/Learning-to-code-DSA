3d7bf4dcb7eb4c509821d51a172956cd411a74f7
Palak <malikpalak1332@gmail.com>
2025-09-30 06:46:13 +0530

    Time: 0 ms (100%), Space: 10.1 MB (68.76%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -5,12 +5,11 @@ public:
         int m=matrix[0].size();
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-            swap(matrix[i][j],matrix[j][i]);
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
         for(int i=0;i<n;i++){
-           reverse(matrix[i].begin(),matrix[i].end());
-            }
+            reverse(matrix[i].begin(),matrix[i].end());
         }
-    
+    }
 };
