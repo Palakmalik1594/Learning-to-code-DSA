6cf51d8c3e2338f0390abe28cf4ca540e4cdd4a6
Palak <malikpalak1332@gmail.com>
2025-12-11 23:32:45 +0530

    Time: 0 ms (100%), Space: 50.3 MB (33.35%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -4,13 +4,13 @@ public:
         int count=0;
         int maxi=0;
         for(int i=0;i<nums.size();i++){
-                if(nums[i]==1){
-                    count++;
-                    maxi=max(maxi,count);
-                }else if(nums[i]==0){
-                    count=0;
-                    
-                }
+            if(nums[i]==1){
+                count++;
+            }
+            maxi=max(maxi,count);
+            if(nums[i]==0){
+                count=0;
+            }
         }
         return maxi;
     }
